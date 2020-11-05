16fe86532e9882fd5449437a7e8502839cdde18e
Aviv Shvitzky <asifaviv@zahav.net.il>
2020-11-05 23:32:17 +0200

    ש

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -13,8 +13,13 @@ const Categories = () => {
       </Navbar>
 
       <div className="page__content">
-        <br/>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti magni consequuntur laborum deserunt, autem eligendi odio labore voluptatem, et, nulla commodi? Quae quos blanditiis quo atque dolore voluptatibus? Reprehenderit, iste?
+        <div class="list-group">
+          <a href="#" class="list-group-item list-group-item-action">Cras justo odio</a>
+          <a href="#" class="list-group-item list-group-item-action">Dapibus ac facilisis in</a>
+          <a href="#" class="list-group-item list-group-item-action">Morbi leo risus</a>
+          <a href="#" class="list-group-item list-group-item-action">Porta ac consectetur ac</a>
+          <a href="#" class="list-group-item list-group-item-action">Vestibulum at eros</a>
+        </div>
       </div>
     </div>
   );
