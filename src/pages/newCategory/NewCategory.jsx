0abffb36d24bb960b1f1c0a6577564ca5d870464
Aviv Shvitzky <asifaviv@zahav.net.il>
2020-11-10 11:02:43 +0200

    a

---
+++ src/pages/newCategory/NewCategory.jsx
@@ -0,0 +1,36 @@
+// libraries and css
+import React, { useEffect } from 'react';
+
+// components
+import Form from '../../components/form/Form'
+
+// consts
+import { NEW_CATEGORY } from '../../consts'
+
+// store
+import { useRecoilState } from "recoil";
+import { currentPage as currentPageAtom } from '../../store/atoms'
+import { useAddCategory } from '../../store/mutations'
+
+
+function NewCategory() {
+  const [currentPage, setCurrentPage] = useRecoilState(currentPageAtom);
+  const addCategory = useAddCategory()
+
+  useEffect(() => {
+    setCurrentPage(NEW_CATEGORY)
+  }, [])
+
+  return (
+    <div className="categories">
+      <div className="page__content">
+        <Form 
+          clickHandler={addCategory}
+          currentPage={currentPage}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default NewCategory;
